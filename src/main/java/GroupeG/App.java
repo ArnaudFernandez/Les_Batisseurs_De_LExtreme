fbca3919717d9aaa46358d1f6ba2ce6b8e81ffd1
SpyLeChaton <arnaud.fernandez1@gmail.com>
2018-02-19 14:32:33 +0100

    Initial commit

---
+++ src/main/java/GroupeG/App.java
@@ -0,0 +1,13 @@
+package GroupeG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
